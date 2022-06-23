96be0a2c4e4ac879c802a0e1803cfd87239b4e66
Gustavo Manca <gustavo.manca@opah.com.br>
2022-06-23 20:55:23 -0300

    feat: add simple calendar with mapped month data

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,17 +1,40 @@
+import { getMappedMonth } from "../utils/date";
+
 function Calendar() {
+  const month = getMappedMonth();
+
   return (
     <div className="container">
       <h1>Calendar</h1>
 
-      <header className="calendar-header">
-        <span className="item">Sunday</span>
-        <span className="item">Monday</span>
-        <span className="item">Tuesday</span>
-        <span className="item">Wednesday</span>
-        <span className="item">Thursday</span>
-        <span className="item">Friday</span>
-        <span className="item">Saturday</span>
-      </header>
+      <div className="calendar">
+        <header className="calendar-header">
+          <span className="item">Sunday</span>
+          <span className="item">Monday</span>
+          <span className="item">Tuesday</span>
+          <span className="item">Wednesday</span>
+          <span className="item">Thursday</span>
+          <span className="item">Friday</span>
+          <span className="item">Saturday</span>
+        </header>
+
+        <div className="calendar-weeks">
+          {month.map((row) =>
+            row.map(({ day, isCurrentMonth, isWeekend }) => (
+              <span
+                className={[
+                  "calendar-day",
+                  isWeekend ? "--weekend" : "",
+                  !isCurrentMonth ? "--disabled" : "",
+                ].join(" ")}
+                key={day}
+              >
+                {day}
+              </span>
+            ))
+          )}
+        </div>
+      </div>
     </div>
   );
 }
