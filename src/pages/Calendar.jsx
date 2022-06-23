812904f59716c2e54da6a64f2ac1c4c81fb6c931
Gustavo Manca <gustavo.manca@opah.com.br>
2022-06-23 17:10:08 -0300

    feat: add initial calendar header markup

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -2,6 +2,16 @@ function Calendar() {
   return (
     <div className="container">
       <h1>Calendar</h1>
+
+      <header className="calendar-header">
+        <span className="item">Sunday</span>
+        <span className="item">Monday</span>
+        <span className="item">Tuesday</span>
+        <span className="item">Wednesday</span>
+        <span className="item">Thursday</span>
+        <span className="item">Friday</span>
+        <span className="item">Saturday</span>
+      </header>
     </div>
   );
 }
