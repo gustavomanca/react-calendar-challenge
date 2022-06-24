1812a93e8b2ce8aa84f721e5736fc8ebd444d9c8
Gustavo Manca <gustavo.manca@opah.com.br>
2022-06-23 22:01:09 -0300

    feat: add modal to calendar page

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,10 +1,62 @@
+import { useState } from "react";
+
+import Modal from "../components/Modal";
+import TextField from "../components/TextField";
 import { getMappedMonth } from "../utils/date";
 
 function Calendar() {
+  const [showModal, setShowModal] = useState(true);
+  const [reminder, setReminder] = useState({
+    title: "",
+    day: "",
+    city: "",
+  });
+
   const month = getMappedMonth();
 
   return (
     <div className="container">
+      {showModal && (
+        <Modal title="New reminder" onClose={() => setShowModal(false)}>
+          <div className="fields-wrapper">
+            <TextField
+              value={reminder.title}
+              placeholder="Type a title..."
+              maxLength={30}
+              onChange={(event) =>
+                setReminder((prev) => ({
+                  ...prev,
+                  title: event.target.value,
+                }))
+              }
+            />
+            <TextField
+              value={reminder.day}
+              placeholder="Type a day..."
+              maxLength={2}
+              onChange={(event) => {
+                const day = event.target.value.match(/\d+/g) ?? "";
+                setReminder((prev) => ({
+                  ...prev,
+                  day,
+                }));
+              }}
+            />
+            <TextField
+              value={reminder.city}
+              placeholder="Type a city..."
+              maxLength={2}
+              onChange={(event) => {
+                setReminder((prev) => ({
+                  ...prev,
+                  city: event.target.value,
+                }));
+              }}
+            />
+          </div>
+        </Modal>
+      )}
+
       <h1>Calendar</h1>
 
       <div className="calendar">
@@ -19,7 +71,7 @@ function Calendar() {
         </header>
 
         <div className="calendar-weeks">
-          {month.map((row) =>
+          {month?.map((row) =>
             row.map(({ day, isCurrentMonth, isWeekend }) => (
               <span
                 className={[
