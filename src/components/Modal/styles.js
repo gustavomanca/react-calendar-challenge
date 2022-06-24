eb83ce87be1aceff9706e24184d5455f566c0f59
Gustavo Manca <gustavo.manca@opah.com.br>
2022-06-24 08:27:06 -0300

    style: add radius do close button

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -1,30 +1,36 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: #fff;
-  height: 400px;
   position: fixed;
   top: 50%;
   left: 50%;
-  z-index: 10;
-  width: 50vw;
+  z-index: 2;
+
   border-radius: 12px;
-  background-color: #fff;
-  transform: translate(-50%, -50%);
-  box-shadow: 0 0 12px rgba(0, 0, 0, 0.1);
+  height: 400px;
   padding: 32px 24px;
+  width: 50vw;
+
+  background-color: #fff;
+  box-shadow: 0 0 12px rgba(0, 0, 0, 0.1);
+
+  transform: translate(-50%, -50%);
 `;
 
 export const Close = styled.button`
   position: absolute;
   right: 0;
   top: 0;
+
   border: none;
-  background-color: transparent;
-  padding: 0;
+  border-radius: 0 12px 0 12px;
   height: 48px;
+  padding: 0;
   width: 48px;
+
+  background-color: transparent;
   cursor: pointer;
+
   transition: background-color 0.3s ease;
   will-change: background-color;
 
@@ -34,10 +40,12 @@ export const Close = styled.button`
     position: absolute;
     left: 50%;
     top: 50%;
-    background-color: #000;
-    width: 16px;
-    height: 2px;
+
     border-radius: 24px;
+    height: 2px;
+    width: 16px;
+
+    background-color: #000;
   }
 
   &::before {
