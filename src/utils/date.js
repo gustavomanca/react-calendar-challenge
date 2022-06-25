d30fccf04eea85af74b31833d3692109416afe13
Gustavo Manca <gustavo.manca@opah.com.br>
2022-06-25 10:32:42 -0300

    refactor: get amount days of month

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,7 @@
 export function getMappedMonth(month = new Date().getMonth()) {
   const year = new Date().getFullYear();
   const weekDayOfFirstDayOfMonth = new Date(year, month, 1).getDay();
+  const amountDays = [];
   let dayCounter = 0 - weekDayOfFirstDayOfMonth;
 
   const mapped = Array.from({ length: 5 }, () =>
@@ -10,8 +11,9 @@ export function getMappedMonth(month = new Date().getMonth()) {
       const day = date.getDate();
       const isWeekend = date.getDay() === 0 || date.getDay() === 6;
       const isCurrentMonth = date.getMonth() === month;
+      if (isCurrentMonth) amountDays.push(day);
       return { day, isCurrentMonth, isWeekend };
     })
   );
-  return mapped;
+  return { mapped, amountDays };
 }
